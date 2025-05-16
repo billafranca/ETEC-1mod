e6a5ed9a2aeee399ddd0d13f49435d8588090f2b
PEDRO_5245 <PEDRO_5245@uirapuru.br>
2025-05-15 22:28:02 -0300

    revisao e aula de php

---
+++ projeto_atividade/exe01/logar.php
@@ -0,0 +1,12 @@
+<?php
+     $nome = $_POST = ['nomeUsuario'];
+     $senha = $_POST = ['senhaUsuario']; 
+
+     if ($nome = "admin" && $senha = "admin"){
+        echo "<h1>olá" $nome, "seja bem vindo!</h1>";
+     }else{
+        echo "<h1>usuário ou senha incorretos</h1>";
+     }
+     
+?>
+ 
