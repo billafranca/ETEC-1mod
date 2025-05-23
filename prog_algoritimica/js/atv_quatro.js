f6f5638c82c10c2682db35187e670cc867533b5b
PEDRO_5245 <PEDRO_5245@uirapuru.br>
2025-05-22 21:12:01 -0300

    a

---
+++ prog_algoritimica/js/atv_quatro.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    var num1 = document.getElementById("n1")
+    var num2 = document.getElementById("n2")
+
+    var sum = document.getElementById("somar")
+    var sub = document.getElementById("subtrair")
+    var mul = document.getElementById("multiplicar")
+    var div = document.getElementById("dividir")
+
+    let resultado = document.getElementById("resultado")
+
+    let numeroUm = number(n1.value)
+
+
+    btn.addEventListener("click", function(){
+        res.textContent("result is: ", res)
+    })
+})
